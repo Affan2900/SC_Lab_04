2a118bd6c387bcece967a191a57fe40de1e1fa2a
Affan2900 <affanamir290@gmail.com>
2024-10-07 16:19:08 +0500

    changes for lab5

--- src/twitter/Filter.java
+++ src/twitter/Filter.java
@@ -2,28 +2,34 @@ package twitter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Filter {
 
-    public static List<Tweet> writtenBy(List<Tweet> tweets, String username) {
-        List<Tweet> result = new ArrayList<>();
-        for (Tweet tweet : tweets) {
-            if (tweet.getAuthor().equalsIgnoreCase(username)) {
-                result.add(tweet);
-            }
-        }
-        return result;
-    }
+	public static List<Tweet> writtenBy(List<Tweet> tweets, String username) {
+	    List<Tweet> result = new ArrayList<>();
+	    for (Tweet tweet : tweets) {
+	        if (isAuthorMatch(tweet.getAuthor(), username)) {
+	            result.add(tweet);
+	        }
+	    }
+	    return result;
+	}
 
-    public static List<Tweet> inTimespan(List<Tweet> tweets, Timespan timespan) {
-        List<Tweet> result = new ArrayList<>();
-        for (Tweet tweet : tweets) {
-            if (timespan.contains(tweet.getTimestamp())) {
-                result.add(tweet);
-            }
-        }
-        return result;
-    }
+	private static boolean isAuthorMatch(String author, String username) {
+	    return author != null && author.equalsIgnoreCase(username);
+	}
+	
+	public static List<Tweet> inTimespan(List<Tweet> tweets, Timespan timespan) {
+	    List<Tweet> result = new ArrayList<>();
+	    for (Tweet tweet : tweets) {
+	        if (tweet.getTimestamp().isAfter(timespan.getStart()) && 
+	            tweet.getTimestamp().isBefore(timespan.getEnd())) {
+	            result.add(tweet);
+	        }
+	    }
+	    return result;
+	}
 
     public static List<Tweet> containing(List<Tweet> tweets, List<String> words) {
         List<Tweet> result = new ArrayList<>();
